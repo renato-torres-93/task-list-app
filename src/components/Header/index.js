04567f101efd1d9471ced32bcddd2333f9929f62
renato-torres-93 <renatotbreis@gmail.com>
2022-06-27 12:00:20 -0300

    Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+function Header ()
+{
+  return (
+    <header>
+      <h2>Menu</h2>
+    </header>
+  )
+}
+export default Header
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import Tasks from './pages/Tasks'
+import Header from './components/Header'
 
 function AppRouter ()
 {
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path='*' element={<NotFound/>}/>
         <Route path='/' element={<Home/>}/>
